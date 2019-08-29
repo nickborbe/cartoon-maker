f4832c4441dbf318032ecb4e3782160c64b9c953
nickborbe <nickborbe@gmail.com>
2019-08-28 20:24:23 -0400

    he talks with keys

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('yo');
 const q = document.getElementById('canvas').getContext('2d');
 
 const closed = new Image();
-closed.src = "./images/closedmouth.png";
+closed.src = "./images/mouthclosed.png";
 const a = new Image();
 a.src = "./images/a.png";
 const o = new Image();
@@ -21,22 +21,25 @@ document.onkeydown = function(e){
 
 
     if(e.key ==="l"){
-
             q.clearRect(100,100,200,200)
             q.drawImage(a, 100, 100, 200, 200);
-      
-
     }
 
     if(e.key ==="k"){
+        q.clearRect(100,100,200,200)
         q.drawImage(o, 100, 100, 200, 200);
         
     }
     if(e.key ==="j"){
+        q.clearRect(100,100,200,200)
         q.drawImage(smile, 100, 100, 200, 200);
         
     }
 
 
 
+}
+
+document.onkeyup = function(){
+    q.drawImage(closed, 100, 100, 200, 200);
 }
